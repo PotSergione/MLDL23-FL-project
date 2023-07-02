3820d97809430a8576fad69caae1929739904b9f
PotSergione <103385978+PotSergione@users.noreply.github.com>
2023-07-02 10:31:33 +0200

    Update FedSR.py

--- models/FedSR.py
+++ models/FedSR.py
@@ -5,36 +5,34 @@ import torch.distributions as distributions
 ### Our main convolutional network
 
 class network(torch.nn.Module):
-
     def __init__(self):
-
         # Various initializations
         super(network, self).__init__()
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+    
         # Classification layers
         self.conv_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(5, 5), device=device)
         self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 5), device=device)
         self.pool = nn.MaxPool2d((2, 2))
         self.act = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
-
+    
         # Fully connected part
         self.fc_1 = nn.Linear(in_features=4*4*64, out_features=2048, device=device)
         self.fc_2 = nn.Linear(in_features=2048, out_features=62, device=device)
         self.z_dim = 1024
-
+    
         # regularizing portion
         # Both those parameters are taken from the GitHub implemetation of the algorithm
         self.L2R_coeff = 0.01 
         self.CMI_coeff = 0.001 
-
+    
         self.r_mu = nn.Parameter(torch.zeros(62,self.z_dim, requires_grad=True, device=device), requires_grad=True)
         self.r_sigma = nn.Parameter(torch.ones(62,self.z_dim, requires_grad=True, device=device), requires_grad=True)
         self.C = nn.Parameter(torch.ones([], requires_grad=True, device=device), requires_grad=True)
         self.criterion = nn.CrossEntropyLoss(reduction='sum')
-      
-    # Training part
+
+    
     def forward(self, x):
 
       x = self.conv_1(x)
@@ -72,7 +70,7 @@ class network(torch.nn.Module):
     def regressive_loss(self, x, y):
 
       """
-      Computes the regularizer term of the FedSR model
+          Computes the regularizer term of the FedSR model
       """
 
       out = self.net(x)
@@ -91,7 +89,6 @@ class network(torch.nn.Module):
           obj = obj + self.L2R_coeff*regL2R
 
       if self.CMI_coeff != 0.0:
-
           # Here we compute the conditional expectation
           r_sigma_softplus = nn.functional.softplus(self.r_sigma)
           r_mu = self.r_mu[y]
